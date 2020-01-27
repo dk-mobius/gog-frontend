a575484c5573c80bdeba73e92de05f4997392fdf
Devin Kendall <devin@dkportfolio.com>
2020-01-27 11:55:24 -0700

    env update

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 
 module.exports = {
@@ -49,6 +50,7 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: './src/index.html',
             filename: './index.html'
-        })
+        }),
+        new Dotenv()
     ]
 };
